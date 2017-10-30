bf8b0a6106076fe9b61d4170210696cd33f4a9e7
londonappbrewery <londonappbrewery@gmail.com>
2017-10-30 11:38:17 +0000

    Initial commit

---
+++ app/src/main/java/com/londonappbrewery/dice_nested_completed/MainActivity.java
@@ -0,0 +1,51 @@
+package com.londonappbrewery.dice_nested_completed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        final ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
+        Button myButton = (Button) findViewById(R.id.rollButton);
+
+        final int[] diceArray = new int[] {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6};
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(6);
+                Log.d("Dicee", "The number is " + number );
+
+                int imageResourceId = diceArray[number];
+                leftDice.setImageResource(imageResourceId);
+
+                number = randomNumberGenerator.nextInt(6);
+                imageResourceId = diceArray[number];
+                rightDice.setImageResource(imageResourceId);
+
+            }
+        });
+
+
+    }
+}
